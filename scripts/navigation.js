084fdaa21bd1ada301fd762e3eca4ff500a8b3e5
Luisa C. Mazzi <Bluellu@users.noreply.github.com>
2017-04-23 05:41:38 -0400

    Initial files
    Bare-bones structure of the web page.

---
+++ scripts/navigation.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/**
+ * @type {object} navigation
+ */
+var navigation = navigation || {};
+
+navigation.ITEMS = { // Name : file
+    'Games' : 'games', 
+    'Resume' : 'resume', 
+    'About Me' : 'about', 
+    'Links' : 'links'
+};
+
+/**
+ * Create main navigation menu and insert it into page
+ */
+navigation.insertNav = function() {
+    let ul = $('<ul></ul>');
+
+    for (var i in this.ITEMS) {
+        // Create a navigation item
+        let li = $('<li id="' + this.ITEMS[i]+ '"><a href="' + this.ITEMS[i] + '.html">'+ i + '</a></li>');
+        li.addClass('deactivated');
+        ul.append(li); 
+        $('#main-nav').append(ul);
+    }
+
+    // Highlight button for the current page
+    let path = window.location.pathname;  
+    let page = ((path.split("/").pop()).split('.'))[0];
+    $('#' + page).removeClass('deactivated').addClass('activated');
+    
+};
+
+// Init function
+navigation.init = function() {
+    this.insertNav();
+};
+
+// Initializing
+$(document).ready(function() {
+    navigation.init();
+});
